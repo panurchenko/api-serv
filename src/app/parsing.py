0245e0371b4248fe2b8bd41669be5bff73e58a88
panurchenko <117811052+panurchenko@users.noreply.github.com>
2022-11-30 14:02:17 +0300

    Update parsing.py

--- src/app/parsing.py
+++ src/app/parsing.py
@@ -7,11 +7,7 @@ async def get_amount(phrase):
     base_url = "https://fast-anime.ru/search/?search"
     param = {'': phrase}
     url = base_url + urllib.parse.urlencode(param)
-    headers = headers = {
-        "Accept": "*/*",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0)"
-    }
-    respond = bs(requests.get(url, headers=headers).text, "html.parser")
+    respond = bs(requests.get(url).text, "html.parser")
     paragraph = respond.find_all('p', class_="title has-text-grey-dark")
     amount = 0
     for x in paragraph[0].text.split():
